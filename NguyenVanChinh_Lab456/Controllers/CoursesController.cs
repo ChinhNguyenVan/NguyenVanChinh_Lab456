3ecbdcc1d3905421d1168113c5655dfeaf2105ab
Chinh Nguyen <chinhht09@gmail.com>
2020-06-04 10:59:09 +0700

    commit sixth

--- NguyenVanChinh_Lab456/Controllers/CoursesController.cs
+++ NguyenVanChinh_Lab456/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using NguyenVanChinh_Lab456.Models;
 using NguyenVanChinh_Lab456.ViewModels;
 
@@ -17,6 +18,7 @@ namespace NguyenVanChinh_Lab456.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -24,6 +26,26 @@ namespace NguyenVanChinh_Lab456.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]  
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturedId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
