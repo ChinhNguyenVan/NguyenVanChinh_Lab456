f4a99f47de9c6e88a2304820c34bfec05fb87f31
Chinh Nguyen <chinhht09@gmail.com>
2020-06-04 10:03:10 +0700

    comit third

--- NguyenVanChinh_Lab456/Models/ApplicationDbContext.cs
+++ NguyenVanChinh_Lab456/Models/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -8,6 +9,9 @@ namespace NguyenVanChinh_Lab456.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
